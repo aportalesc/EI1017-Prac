b358801879396ccef4416fe4f0407e55e32402f1
aportalesc <98490578+aportalesc@users.noreply.github.com>
2022-02-26 12:50:13 +0100

    Actualizada la clase Statistics
    Se han actualizado los métodos de Statistics para que funcionen con Collection, en vez de arrays, de esta forma estos métodos se pueden utilizar con todo tipo de colecciones.

--- src/main/java/Statistics/Statistics.java
+++ src/main/java/Statistics/Statistics.java
@@ -1,29 +1,31 @@
 package Statistics;
 
+import java.util.Collection;
+
 public class Statistics {
 
-    public double mediaAritmetica(double[] c){
-        if(c.length == 0)
+    public static double mediaAritmetica(Collection<Double> c){
+        if(c.size() == 0)
             return 0;
         double suma = 0;
         for(Double elemento : c)
             suma += elemento;
-        return suma/c.length;
+        return suma/c.size();
     }
 
-    public double varianza(double[] c){
-        if(c.length == 0)
+    public static double varianza(Collection<Double> c){
+        if(c.size() == 0)
             return 0;
         double sumatorio = 0;
         double media = mediaAritmetica(c);
         for(Double elemento : c)
             sumatorio += Math.pow(elemento - media, 2);
 
-        return sumatorio/c.length;
+        return sumatorio/c.size();
     }
 
-    public double desviacionTipica(double[] c){
-        if(c.length == 0)
+    public static double desviacionTipica(Collection<Double> c){
+        if(c.size() == 0)
             return 0;
         return Math.sqrt(varianza(c));
     }
--- src/test/java/Statistics/StatisticsTest.java
+++ src/test/java/Statistics/StatisticsTest.java
@@ -1,46 +1,66 @@
 package Statistics;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsTest {
 
-    double[] c0 = {1};
-    double[] c1 = {5,5,5,5,5};
-    double[] c2 = {1,2,3,4};
-    double[] c3 = {1,2,-2,-1};
-    double[] c4 = {};
-    Statistics est = new Statistics();
+    Collection<Double> c0 = new ArrayList<>();
+    Collection<Double> c1 = new ArrayList<>();
+    Collection<Double> c2 = new ArrayList<>();
+    Collection<Double> c3 = new ArrayList<>();
+    Collection<Double> c4 = new ArrayList<>();
 
+
+    StatisticsTest(){
+        c0.add(1.0);
+        c1.add(5.0);
+        c1.add(5.0);
+        c1.add(5.0);
+        c1.add(5.0);
+        c1.add(5.0);
+        c2.add(1.0);
+        c2.add(2.0);
+        c2.add(3.0);
+        c2.add(4.0);
+        c3.add(1.0);
+        c3.add(2.0);
+        c3.add(-2.0);
+        c3.add(-1.0);
+    }
     @Test
     @DisplayName("Test de la media aritmética")
     void mediaAritmeticaTest() {
-        assertEquals(1, est.mediaAritmetica(c0));
-        assertEquals(5, est.mediaAritmetica(c1));
-        assertEquals(2.5, est.mediaAritmetica(c2));
-        assertEquals(0, est.mediaAritmetica(c3));
-        assertEquals(0, est.mediaAritmetica(c4));
+        assertEquals(1, Statistics.mediaAritmetica(c0));
+        assertEquals(5, Statistics.mediaAritmetica(c1));
+        assertEquals(2.5, Statistics.mediaAritmetica(c2));
+        assertEquals(0, Statistics.mediaAritmetica(c3));
+        assertEquals(0, Statistics.mediaAritmetica(c4));
     }
 
     @Test
     @DisplayName("Test de la varianza")
     void varianza() {
-        assertEquals(0, est.varianza(c0));
-        assertEquals(0, est.varianza(c1));
-        assertEquals(1.25, est.varianza(c2));
-        assertEquals(2.5, est.varianza(c3));
-        assertEquals(0, est.varianza(c4));
+        assertEquals(0, Statistics.varianza(c0));
+        assertEquals(0, Statistics.varianza(c1));
+        assertEquals(1.25, Statistics.varianza(c2));
+        assertEquals(2.5, Statistics.varianza(c3));
+        assertEquals(0, Statistics.varianza(c4));
     }
 
     @Test
     @DisplayName("Test de la desviación típica")
     void desviacionTipica() {
-        assertEquals(0, est.desviacionTipica(c0));
-        assertEquals(0, est.desviacionTipica(c1));
-        assertEquals(1.118, est.desviacionTipica(c2), 0.001);
-        assertEquals(1.581, est.desviacionTipica(c3), 0.001);
-        assertEquals(0, est.desviacionTipica(c4));
+        assertEquals(0, Statistics.desviacionTipica(c0));
+        assertEquals(0, Statistics.desviacionTipica(c1));
+        assertEquals(1.118, Statistics.desviacionTipica(c2), 0.001);
+        assertEquals(1.581, Statistics.desviacionTipica(c3), 0.001);
+        assertEquals(0, Statistics.desviacionTipica(c4));
     }
 }
