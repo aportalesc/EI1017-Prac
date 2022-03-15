74f0cb14caf987167a119c524e1b02f3e4cae439
aportalesc <98490578+aportalesc@users.noreply.github.com>
2022-03-15 19:19:09 +0100

    Añadida la clase Kmeans

---
+++ src/main/java/Algorithms/Kmeans.java
@@ -0,0 +1,73 @@
+package Algorithms;
+
+import CSVread.Row;
+import CSVread.Table;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Kmeans {
+
+    private int numberClusters;
+    private int iterations;
+    private long seed;
+    private List<Row> centr;
+
+    public Kmeans(int numberClusters, int iterations, long seed){
+        super();
+        this.numberClusters = numberClusters;
+        this.iterations = iterations;
+        this.seed = seed;
+        this.centr = new ArrayList<>();
+    }
+
+    public void train(Table data){
+        List<List<Row>> clusters = new ArrayList<>();
+        for(int i = 0; i < numberClusters; i++) {
+            centr.add(data.getRowAt(i));
+            clusters.add(new LinkedList<>());
+        }
+
+        for(int n = 0; n < iterations; n++){
+
+            for(int i = 0; i < data.getSize(); i++){
+                Row elem = data.getRowAt(i);
+                int ncluster = closestCluster(elem);
+                clusters.get(ncluster).add(elem);
+            }
+
+
+
+
+
+
+
+        }
+
+
+    }
+
+    private double euclideanDist(Row r1, Row r2) {
+        double dist = 0;
+        for(int i = 0; i < r1.size(); i++)
+            dist += Math.pow(r1.getData().get(i) - r2.getData().get(i), 2);
+        dist = Math.sqrt(dist);
+        return dist;
+    }
+
+    private int closestCluster(Row elem){
+        double min = Double.MAX_VALUE;
+        double dist = 0.0;
+        int ncluster = 0;
+        for(int i = 0; i < centr.size(); i++){
+            dist = euclideanDist(centr.get(i), elem);
+            if(dist < min){
+                min = dist;
+                ncluster = i;
+            }
+        }
+        return ncluster;
+    }
+
+}
